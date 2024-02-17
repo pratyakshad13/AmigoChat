c2f01d00fb6b22978097617cd83d579d75d4a283
Pratyaksha Dwivedi <pratyakshad13@gmail.com>
2024-02-17 12:18:19 +0530

    logic added for various functionalities

--- main.js
+++ main.js
@@ -1,1 +1,85 @@
+let APP_ID="YOUR-ID"
+let token=null;
+let uid=String(Math.floor(Math.random()*10000));
 
+let client;
+let channel;
+
+let localStream;
+let remoteStream;
+let peerConnection;
+
+const servers= {
+    iceServers:[
+        {
+            urls:['stun:stun1.l.google.com:19302','stun:stun2.l.google.com:19302']
+        }
+    ]
+}
+
+let init= async ()=>{
+
+    client= await AgoraRTM.createInstance(APP_ID);
+    await client.login({uid,token});
+
+    channel= client.createChannel('main');
+    await channel.join();
+
+    channel.on('MemberJoined',handleUserJoin);
+
+    client.on('MessageFromPeer',handleMessageFromPeer);
+
+    localStream=await navigator.mediaDevices.getUserMedia({video:true,audio:false})
+    document.getElementById('user-1').srcObject =localStream
+
+   
+}
+
+let handleMessageFromPeer= async(message,MemberId)=>{
+    message=JSON.parse(message.text)
+    console.log('Message:',message)
+}
+
+let handleUserJoin =async(MemberId)=>{
+    console.log('A new USer Joined the channel:',MemberId);
+    createOffer(MemberId)
+}
+
+
+let createOffer= async (MemberId)=>{
+    peerConnection=new RTCPeerConnection(servers);
+
+    remoteStream= new MediaStream()
+    document.getElementById('user-2').srcObject= remoteStream;
+
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track,localStream)
+    });
+
+    peerConnection.onTrack =(event)=>{
+        event.stream[0].getTracks().forEach((track)=>{
+            remoteStream.addTrack(track);
+        })
+    }
+
+    peerConnection.onicecandidate =async (event)=>{
+        if(event.candidate){
+            client.sendMessageToPeer({text:JSON.stringify({'type':'candidate','candidate':event.candidate})},MemberId)
+        }
+    }
+
+
+    let offer=await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'offer','offer':offer})},MemberId)
+}
+
+
+
+
+
+
+
+init()
